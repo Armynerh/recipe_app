07a80a0c6c60f07816b9c871131a507bb603e48f
Armynerh <aimenah@outlook.com>
2023-11-29 18:53:50 -0500

    create initial files for db, login and registeration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :food, dependent: :destroy
-  has_many :recipe, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
