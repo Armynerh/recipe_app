c6961072b3f789ebd948d6e3b61380b3cf2569b9
Armynerh <aimenah@outlook.com>
2023-11-29 21:08:50 -0500

    modify db files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_232234) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_003447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,10 +40,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_232234) do
     t.integer "preparation_time"
     t.integer "cooking_time"
     t.text "description"
-    t.boolean "public"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "public"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
