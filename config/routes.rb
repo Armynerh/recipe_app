7d0a14e595fd2ca9715a2e4dac047e4c34167066
Armynerh <aimenah@outlook.com>
2023-11-29 22:10:53 -0500

    fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
   # Defines the root path route ("/")
-  root "recipes#index"
+ 
   resources :foods
   get '/general_shopping_list', to: 'general_shopping_list#index'
   resources :public_recipes, only: [:index]
@@ -16,4 +20,5 @@ Rails.application.routes.draw do
       post 'toggle_public'
     end
   end
+  root "recipes#index"
 end
