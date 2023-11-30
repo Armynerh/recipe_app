53b3e2f2c88680b95339c25e93934602f057737d
Armynerh <aimenah@outlook.com>
2023-11-30 11:56:44 -0500

    fix recipe delete

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,42 @@
 class FoodsController < ApplicationController
+  class FoodsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      @foods = current_user.foods
+    end
+
+    def show
+      @food = current_user.foods.find(params[:id])
+    end
+
+    def new
+      @food = current_user.foods.build
+    end
+
+    def create
+      @food = current_user.foods.build(food_params)
+      if @food.save
+        redirect_to @food, notice: 'Food was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @food = current_user.foods.find(params[:id])
+    end
+
+    def destroy
+      @food = current_user.foods.find(params[:id])
+      @food.destroy
+      redirect_to foods_url, notice: 'Food was successfully destroyed.'
+    end
+
+    private
+
+    def food_params
+      params.require(:food).permit(:name, :unit, :price, :quantity)
+    end
+  end
 end
