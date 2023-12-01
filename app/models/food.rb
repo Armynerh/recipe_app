9da32e36f194827e2284434789293900fce2782c
Kehinde Aminat <aimenah@outlook.com>
2023-12-01 15:09:32 -0500

    Revert "Feature/create db login and reg"

--- app/models/food.rb
+++
@@ -1,7 +0,0 @@
-class Food < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_foods, dependent: :destroy
-
-  validates :name, :unit, :price, :quantity, presence: true
-  validates :price, :quantity, numericality: { only_integer: true }
-end
